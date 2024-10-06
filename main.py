190e771ff3b2b231b2e4025a8c9003aeec4547ef
Nitsan BenHanoch <nitsanbh@gmail.com>
2024-10-06 18:23:57 +0300

    added transparency_mode feature

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from PIL import Image
 
+from typing import Literal, TypeAlias, Optional
+
 from shutil import get_terminal_size
 from sys import argv
+import re
 
 import urllib.request
 from io import BytesIO
@@ -35,7 +38,9 @@ def resize(img: Image.Image, desired_width: int) -> Image.Image:
   return img.resize((new_width, new_height))
 
 
-def to_pixels_array(img: Image.Image) -> list[list[tuple[int, int, int]]]:
+PixelsArray: TypeAlias = list[list[tuple[int, int, int, int]]]
+
+def to_pixels_array(img: Image.Image) -> PixelsArray:
   width, height = img.size
   pixels = list(img.getdata())
   return [pixels[i * width : (i + 1) * width] for i in range(height)]
@@ -73,25 +78,38 @@ def parse_argv() -> tuple[list[str], dict[str, str]]:
   return regular_args, kv_args
 
 
+def print_help_and_exit():
+  print()
+  print(f'usage: python3 {argv[0]} path/to/image [--width] [--alpha]')
+  print()
+  print(f'  path/to/image   local file, or remote url')
+  print(f'  --width         number of terminal columns, or a percentage')
+  print(f'  --alpha         transparency mode: `threshold` |`whiten` |`blacken` ')
+  print()
+  exit()
+
+
 def assert_cmd_args() -> tuple[str, dict[str, str]]:
   regular_args, kv_args = parse_argv()
-  optional_keys = ['--width']
-      
+  optional_keys = ('--width', '--alpha')
+  
   if len(regular_args) != 1 or kv_args.keys() - optional_keys:
-    print()
-    print(f'usage: python3 {argv[0]} path/to/image [--width]')
-    print()
-    print(f'  path/to/image   local file, or remote url')
-    print(f'  --width         number of terminal columns, or a percentage')
-    print()
-    exit(1)
+    print_help_and_exit()
+  
+  width_arg = kv_args.get('--width', None)
+  if width_arg is not None and not re.fullmatch(r'\d+%?', width_arg):
+    print_help_and_exit()
+  
+  alpha_arg = kv_args.get('--alpha', None)
+  if alpha_arg is not None and alpha_arg not in ('threshold', 'whiten', 'blacken'):
+    print_help_and_exit()
   
   img_path = regular_args[0]
   return img_path, kv_args
 
 
 def get_desired_width(kv_args: dict[str, str]) -> int:
-  width_arg = kv_args.get('--width', '')
+  width_arg = kv_args.get('--width', None)
   
   if width_arg and not width_arg.endswith('%'):
     return int(width_arg)
@@ -105,9 +123,22 @@ def get_desired_width(kv_args: dict[str, str]) -> int:
   return terminal_width
 
 
-def ansi_color(r, g, b, alpha) -> str:
-  if alpha < 32:
-    return '\033[0m'
+TransparencyMode = Literal['threshold', 'whiten', 'blacken']
+
+def get_desired_transparency_mode(kv_args: dict[str, str]) -> TransparencyMode:
+  return kv_args.get('--alpha', 'whiten')
+
+
+def ansi_color(r, g, b, alpha = 255, transparency_mode: Optional[TransparencyMode] = None) -> str:
+  if alpha < 255:
+    if transparency_mode == 'threshold':
+      if alpha < 32: return '\033[0m'
+    else:
+      bg_r, bg_g, bg_b = (0, 0, 0) if transparency_mode == 'blacken' else (255, 255, 255)
+      r = round(r * alpha / 255 + bg_r * (255 - alpha) / 255)
+      g = round(g * alpha / 255 + bg_g * (255 - alpha) / 255)
+      b = round(b * alpha / 255 + bg_b * (255 - alpha) / 255)
+      return ansi_color(r, g, b)
   
   if supports_true_color:
     return f'\033[48;2;{r};{g};{b}m'
@@ -115,28 +146,31 @@ def ansi_color(r, g, b, alpha) -> str:
   # fallback to older ansi 256-color palette
   # colors 16 to 231 represent a 6×6×6 color cube, and colors 232 to 255 are grayscale shades.
   if r == g == b:
-    color_index = 232 + round(r / 255 * 23)
+    color_code = 232 + round(r / 255 * 23)
   else:
     r6, g6, b6 = round(r / 255 * 5), round(g / 255 * 5), round(b / 255 * 5)
-    color_index = 16 + (36 * r6) + (6 * g6) + b6
-  return f'\033[48;5;{color_index}m'
+    color_code = 16 + (36 * r6) + (6 * g6) + b6
+  return f'\033[48;5;{color_code}m'
 
 
-def print_image(pixels: list[list[tuple[int, int, int]]]) -> None:
+def print_image(pixels: PixelsArray, transparency_mode: TransparencyMode) -> None:
   print()
   for row in pixels:
-    chars = (ansi_color(*rgba) + '  ' for rgba in row)
+    chars = (ansi_color(*rgba, transparency_mode) + '  ' for rgba in row)
     print(''.join(chars) + '\033[0m')
   print()
 
 
 def main() -> None:
   image_path, kv_args = assert_cmd_args()
+  
   desired_width = get_desired_width(kv_args)
+  transparency_mode = get_desired_transparency_mode(kv_args)
+  
   img = load_image(image_path)
   img = resize(img, desired_width)
   pixels = to_pixels_array(img)
-  print_image(pixels)
+  print_image(pixels, transparency_mode)
 
 
 if __name__ == '__main__':
